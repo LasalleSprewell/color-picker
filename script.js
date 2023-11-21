9a92cfbfb4d745e479e6fdcb39ffbb28b48ec2a8
LasalleSprewell <133616710+LasalleSprewell@users.noreply.github.com>
2023-11-21 04:56:15 -0500

    Completed simple color and gradient color picker

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
-// // This block of code is to setup the Simple Color logic
-// The background of the page will change according to a list of colors in the simpleColors Array
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the simpleButton exists on the page
+    const simpleButton = document.getElementById("btn-simple");
+    if (simpleButton) {
+        simpleButton.addEventListener("click", function() {
+            const randomNum = simpleRandom();
+            document.body.style.backgroundColor = simpleColors[randomNum];
+              color.textContent = simpleColors[randomNum];
+          });
+    }
 
+    // Check if the gradButton exists on the page
+    const gradButton = document.getElementById("btn-gradient");
+    if (gradButton) {
+        gradButton.addEventListener("click", function() {
+            const gradientVal = gradient();
+            document.body.style.background = gradientVal;
+            color1.textContent = getRandomHexCode();
+            color2.textContent = getRandomHexCode();
+        });
+    }
+
+});
 const color = document.getElementById("color");
 const simpleColors = ["red","green","blue","purple","orange","yellow"];
 const simpleButton = document.getElementById("btn-simple");
 function simpleRandom() {
     return Math.floor (Math.random() * simpleColors.length);
 }
-simpleButton.addEventListener("click", function(){
-  console.log(simpleRandom());
-  const randomNum = simpleRandom();
-  document.body.style.backgroundColor = simpleColors[randomNum];
-color.textContent = simpleColors[randomNum];
+
+
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+
+const gradButton = document.getElementById("btn-gradient");
+gradButton.addEventListener("click", function(){
+    const gradientVal = gradient()
+    document.body.style.background = gradientVal;
+    color1.textContent = getRandomHexCode();
+    color2.textContent = getRandomHexCode();
 });
 
+function getRandomHexCode() {
+    const characters = '0123456789ABCDEF';
+    let hexCode = '#';
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        hexCode += characters[randomIndex];
+    }
 
+    return hexCode;
+};
+
+function gradient() {
+    let color1 = getRandomHexCode();
+    let color2 = getRandomHexCode();
+    while (color1 === color2) {
+        color2 = getRandomHexCode();
+    }
+    let newGradient = `linear-gradient(to right, ${color1}, ${color2})`;
+    return newGradient;
+
+
+};
